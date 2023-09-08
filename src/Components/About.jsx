634dc2b37f96a25764a83fd12ed2e5d8d252a717
Nebiyu Getachew Gelaw <99326586+nebiyu-ethio@users.noreply.github.com>
2023-09-08 12:21:15 +0300

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/stat2.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "sky background beautiful and simple";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Cloud Security student studying at Addis Ababa Science and Technology University. I enjoy creating unique and secure cloud architecture in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Cloud Architect",
+  "Cloud Security",
+  "Architect Design",
+  "Security testing",
+  "JavaScript",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "As a Cloud Security Engineer, I am responsible for designing, implementing, and maintaining the security of my organization’s cloud computing environment. I use my expertise in cloud technology to recognize and stop threats to our cloud systems, ensure security controls are properly implemented and configured, and create cloud-based programs and systems. My role includes identifying and mitigating potential cloud-based security risks, developing and implementing security controls for the cloud, and ensuring compliance with relevant security regulations and standards. I am dedicated to keeping our cloud environment secure and protected against cyber threats.";
 
 const About = () => {
   return (
